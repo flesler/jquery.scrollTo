0635bbf17ebddc3e0ace77827c91429f20f2ccee
Ariel Flesler <aflesler@gmail.com>
2013-11-29 17:32:23 -0300

    Changed license to MIT and added in repo
    Bumped to 1.4.7

--- jquery.scrollTo.js
+++ jquery.scrollTo.js
@@ -1,54 +1,12 @@
 /*!
  * jQuery.ScrollTo
  * Copyright (c) 2007-2013 Ariel Flesler - aflesler<a>gmail<d>com | http://flesler.blogspot.com
- * Dual licensed under MIT and GPL.
+ * Licensed under MIT
  *
  * @projectDescription Easy element scrolling using jQuery.
  * http://flesler.blogspot.com/2007/10/jqueryscrollto.html
  * @author Ariel Flesler
  * @version 1.4.7
-
- * @id jQuery.scrollTo
- * @id jQuery.fn.scrollTo
- * @param {String, Number, DOMElement, jQuery, Object} target Where to scroll the matched elements.
- *	  The different options for target are:
- *		- A number position (will be applied to all axes).
- *		- A string position ('44', '100px', '+=90', etc ) will be applied to all axes
- *		- A jQuery/DOM element ( logically, child of the element to scroll )
- *		- A string selector, that will be relative to the element to scroll ( 'li:eq(2)', etc )
- *		- A hash { top:x, left:y }, x and y can be any kind of number/string like above.
- *		- A percentage of the container's dimension/s, for example: 50% to go to the middle.
- *		- The string 'max' for go-to-end. 
- * @param {Number, Function} duration The OVERALL length of the animation, this argument can be the settings object instead.
- * @param {Object,Function} settings Optional set of settings or the onAfter callback.
- *	 @option {String} axis Which axis must be scrolled, use 'x', 'y', 'xy' or 'yx'.
- *	 @option {Number, Function} duration The OVERALL length of the animation.
- *	 @option {Boolean} interrupt If true, the scrolling animation will stop on user scroll
- *	 @option {String} easing The easing method for the animation.
- *	 @option {Boolean} margin If true, the margin of the target element will be deducted from the final position.
- *	 @option {Object, Number} offset Add/deduct from the end position. One number for both axes or { top:x, left:y }.
- *	 @option {Object, Number} over Add/deduct the height/width multiplied by 'over', can be { top:x, left:y } when using both axes.
- *	 @option {Boolean} queue If true, and both axis are given, the 2nd axis will only be animated after the first one ends.
- *	 @option {Function} onAfter Function to be called after the scrolling ends. 
- *	 @option {Function} onAfterFirst If queuing is activated, this function will be called after the first scrolling ends.
- * @return {jQuery} Returns the same jQuery object, for chaining.
- *
- * @desc Scroll to a fixed position
- * @example $('#container').scrollTo( 340 );
- *
- * @desc Scroll relatively to the actual position
- * @example $('#container').scrollTo( '+=340px', { axis:'y' } );
- *
- * @desc Scroll using a selector (relative to the scrolled element)
- * @example $(window).scrollTo( 'p.paragraph:eq(2)', 500, { easing:'swing', queue:true, axis:'xy' } );
- *
- * @desc Scroll to a DOM element (same for jQuery object)
- * @example $(window).scrollTo( document.getElementById('element'), { duration:500, axis:'x', onAfter:function() {
- *				alert('scrolled!!');																   
- *			}});
- *
- * @desc Scroll on both axes, to different values
- * @example $('div').scrollTo( { top: 300, left:'+=200' }, { axis:'xy', offset:-20 } );
  */
 
 ;(function( $ ) {
